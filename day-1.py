06c15359c5d53f6f4d9c59fdeadcdc92bec357db
Matthew <22771068+MeM5@users.noreply.github.com>
2020-12-02 15:48:03 -0500

    Day 1 Complete

---
+++ day-1.py
@@ -0,0 +1,15 @@
+input = open('inputs/' + __file__.split('\\')[-1].split('.')[0] + '.txt', 'r')
+input_lines = [int(line.rstrip()) for line in input]
+
+#part 1
+for line in input_lines: 
+    if (2020-line) in input_lines: 
+        print('part 1: ' + str(line * (2020-line)))
+        break
+
+#part 2
+for line in input_lines:
+    for sub_line in input_lines:
+        if (2020-line-sub_line) in input_lines:
+            print('part 2: ' + str(line * sub_line * (2020-line-sub_line)))
+            quit()
