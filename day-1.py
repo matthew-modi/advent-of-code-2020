695a5cf00d47125edf46c3f8da45831bc6036e37
Matthew <22771068+MeM5@users.noreply.github.com>
2020-12-02 16:12:42 -0500

    Day 2 Complete

--- day-1.py
+++ day-1.py
@@ -1,13 +1,13 @@
 input = open('inputs/' + __file__.split('\\')[-1].split('.')[0] + '.txt', 'r')
 input_lines = [int(line.rstrip()) for line in input]
 
-#part 1
+# part 1
 for line in input_lines: 
     if (2020-line) in input_lines: 
         print('part 1: ' + str(line * (2020-line)))
         break
 
-#part 2
+# part 2
 for line in input_lines:
     for sub_line in input_lines:
         if (2020-line-sub_line) in input_lines:
---
+++ day-2.py
@@ -0,0 +1,19 @@
+input = open('inputs/' + __file__.split('\\')[-1].split('.')[0] + '.txt', 'r')
+input_lines = [{'freq_min': int(line.rstrip().split(' ')[0].split('-')[0]), 
+                'freq_max': int(line.rstrip().split(' ')[0].split('-')[1]), 
+                'letter': line.rstrip().split(' ')[1].split(':')[0], 
+                'password': line.rstrip().split(' ')[2]} for line in input]
+
+# part 1
+valid = 0
+for line in input_lines:
+    if line['freq_min']<=line['password'].count(line['letter'])<=line['freq_max']:
+        valid += 1
+print('part 1: ' + str(valid))
+
+# part 2
+valid = 0
+for line in input_lines:
+    if (line['password'][line['freq_min']-1] == line['letter']) != (line['password'][line['freq_max']-1] == line['letter']):
+        valid += 1
+print('part 2: ' + str(valid))
