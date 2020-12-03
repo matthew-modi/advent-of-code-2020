2e234201342ef2f776036c2d18e32d7e0f22701e
Matthew <22771068+MeM5@users.noreply.github.com>
2020-12-03 00:59:55 -0500

    Update Day 3

--- day-3.py
+++ day-3.py
@@ -1,12 +1,16 @@
+import time
+start = time.time()
+
+
 input = open('inputs/' + __file__.split('\\')[-1].split('.')[0] + '.txt', 'r')
 tree_rows = [list(line.rstrip()) for line in input]
-
+width = len(tree_rows[0])
 
 # part 1
 trees = 0
 x = 0
 for y in range(len(tree_rows)):
-    if tree_rows[y][x%31] == '#':
+    if tree_rows[y][x%width] == '#':
         trees += 1
     x+=3
 
@@ -29,7 +33,7 @@ for slopes in slope_set:
     for y in range(len(tree_rows)):
         if slopes[1]*y > len(tree_rows):
             break
-        if tree_rows[slopes[1]*y][x%31] == '#':
+        if tree_rows[slopes[1]*y][x%width] == '#':
             trees += 1
         x += slopes[0]
     results.append(trees)
@@ -38,3 +42,7 @@ total = 1
 for num in results:
     total = num * total
 print('part 2: ' + str(total))
+
+
+end = time.time()
+print('complete in ' + str(end-start) + ' seconds')
